6e485ab43d7f82d0d9886cb29dbb282955c41515
Chris Anderson <jchris@gmail.com>
2023-05-05 18:39:39 -0700

    game board

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,117 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client'
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
 
-export default function Home() {
+const Container = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+`
+
+const Sidebar = styled.div`
+  flex: 1;
+  background-color: #f0f0f0;
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`
+
+const Button = styled.button`
+  padding: 0.5rem 1rem;
+  font-size: 1rem;
+  cursor: pointer;
+`
+
+const GameBoard = styled.div`
+  flex: 3;
+  position: relative;
+  height: 100%;
+`
+
+const TerrainSquare = styled.div<{ scale: number }>`
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: ${({ scale }) => `${scale}rem`};
+  z-index: ${({ scale }) => 9 - scale}; // Reversed z-index
+`
+
+interface TerrainSquareData {
+  scale: number
+  emoji: string
+}
+
+const App: React.FC = () => {
+  const [terrainLayer, setTerrainLayer] = useState<TerrainSquareData[][]>([])
+
+  useEffect(() => {
+    const newTerrainLayer: TerrainSquareData[][] = [
+      new Array(4096), // 1x scale sublayer
+      new Array(1024), // 2x scale sublayer
+      new Array(256), // 4x scale sublayer
+      new Array(64) // 8x scale sublayer
+    ]
+
+    // Populate the terrain layer
+    for (let i = 0; i < newTerrainLayer.length; i++) {
+      const scale = Math.pow(2, i)
+      for (let j = 0; j < newTerrainLayer[i].length; j++) {
+        const randomEmoji = getRandomTerrainEmoji()
+        newTerrainLayer[i][j] = {
+          scale,
+          emoji: randomEmoji
+        }
+      }
+    }
+
+    setTerrainLayer(newTerrainLayer)
+  }, [])
+
+  function getRandomTerrainEmoji(): string {
+    if (Math.random() < 0.1) return ""
+    const terrainEmojis = ['⛰️', '🌋', '🌄', '🌲', '🌳', '🏜️', '🏖️', '🌊', '🏝️', '❄️']
+    const randomIndex = Math.floor(Math.random() * terrainEmojis.length)
+    return terrainEmojis[randomIndex]
+  }
+
+  const boardWidth = window.innerWidth * (3 / 4)
+  const boardHeight = window.innerHeight
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <Container>
+      <Sidebar>
+        <Button>Edit Terrain</Button>
+        <Button>Play Game</Button>
+        <Button>Share Game</Button>
+      </Sidebar>
+      <GameBoard>
+        {terrainLayer.flatMap((sublayer, sublayerIndex) => {
+          const squaresPerRow = Math.sqrt(sublayer.length)
+          const squareSize = (boardHeight / squaresPerRow) * sublayer[0].scale
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+          return sublayer.map((square, squareIndex) => {
+            const rowIndex = Math.floor(squareIndex / squaresPerRow)
+            const colIndex = squareIndex % squaresPerRow
 
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+            const xPos = colIndex * (boardHeight / squaresPerRow);
+            const yPos = rowIndex * (boardHeight / squaresPerRow);
+            
+            return (
+              <TerrainSquare
+                key={`${sublayerIndex}-${squareIndex}`}
+                scale={square.scale}
+                style={{ left: xPos, top: yPos }}
+              >
+                {square.emoji}
+              </TerrainSquare>
+            )
+          })
+        })}
+      </GameBoard>
+    </Container>
   )
 }
+
+export default App
